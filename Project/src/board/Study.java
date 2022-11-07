3cacbc8cdda13b62dbde07c8f4ab42b67a207df1
KimJaeHo <116423185+MMMoonte@users.noreply.github.com>
2022-11-08 05:09:25 +0900

    Add files via upload

---
+++ Project/src/board/Study.java
@@ -0,0 +1,78 @@
+package board;
+
+import java.sql.Date;
+
+public class Study {
+
+	private int number = 0;
+	private String nickName = "";
+	private String title = "";
+	private Date promiseTime;
+	private Date writeTime;
+	private String count = "";
+	private String sex = "";
+	private String dest = "";
+	private String main = "";
+	private String category = "";
+	
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getPromiseTime() {
+		return promiseTime;
+	}
+	public void setPromiseTime(Date promiseTime) {
+		this.promiseTime = promiseTime;
+	}
+	public Date getWriteTime() {
+		return writeTime;
+	}
+	public void setWriteTime(Date writeTime) {
+		this.writeTime = writeTime;
+	}
+	public String getCount() {
+		return count;
+	}
+	public void setCount(String count) {
+		this.count = count;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getDest() {
+		return dest;
+	}
+	public void setDest(String dest) {
+		this.dest = dest;
+	}
+	public String getMain() {
+		return main;
+	}
+	public void setMain(String main) {
+		this.main = main;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+}
