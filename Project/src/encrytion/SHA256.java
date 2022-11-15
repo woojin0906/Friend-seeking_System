ec54582e3587bd155139f6c448d0221511e8fd02
gomwoong1 <gomwoong0619@naver.com>
2022-11-15 18:16:07 +0900

    주석 추가

--- Project/src/encrytion/SHA256.java
+++ Project/src/encrytion/SHA256.java
@@ -5,6 +5,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class SHA256 {
 	
+	// SHA256으로 암호화하는 메서드
 	public String enc256(String pwd) throws NoSuchAlgorithmException {
 		
 		MessageDigest md = MessageDigest.getInstance("SHA-256");
@@ -13,6 +14,8 @@ public class SHA256 {
 		return bytesToHex(md.digest());
 	}
 	
+	
+	// 바이트 형식의 문자열을 16진수 문자열로 변환해주는 메서드
 	private String bytesToHex(byte[] bytes) {
         StringBuilder builder = new StringBuilder();
         for (byte b : bytes) {
